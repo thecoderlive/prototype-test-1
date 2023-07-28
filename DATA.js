de95da988cff6fdb3d9e8ac552bdcc02ee1f440d
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-07-28 13:12:32 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "98ebf780-c31a-49f0-8539-4b73be7a150a",
+    "home_feed": [
+        {
+            "id": "bceb8c11-2534-471f-a3ed-e0efbc993994",
+            "artist_name": "skill",
+            "home_carousel": [
+                {
+                    "id": "a5304ccc-ae8b-4c6e-96ee-52718a5a17c6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/52.png"
+                },
+                {
+                    "id": "28f713dc-a707-43a1-91ae-0ff143e0e2b0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/303.png"
+                },
+                {
+                    "id": "b02695ce-aea4-4ab2-af96-0096ddb37e8b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/338.png"
+                },
+                {
+                    "id": "866027c0-feeb-407e-8b2d-9527ade9a647",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/279.png"
+                },
+                {
+                    "id": "61653cf2-c609-4bb6-8369-17adb78a5580",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/13.png"
+                }
+            ],
+            "art_caption": "for",
+            "art_description": "Discuss box us especially politics table final goal. Back never story talk on stay year."
+        },
+        {
+            "id": "214485a4-6096-46a2-9166-163a715c67a0",
+            "artist_name": "stand",
+            "home_carousel": [
+                {
+                    "id": "383d55a3-fc17-4aef-8dc1-0d13c9f8af1c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/279.png"
+                },
+                {
+                    "id": "a5a7da1c-2e37-4eb6-ae59-d1538cd03370",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/303.png"
+                },
+                {
+                    "id": "bd673aff-78f5-4c47-9647-3fed68f2c8a4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/254.png"
+                },
+                {
+                    "id": "1ab3c885-f6b9-4ddb-a913-e50fe3f03653",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/60.png"
+                },
+                {
+                    "id": "40874849-0b19-40a9-b722-2f233bb86032",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/134.png"
+                }
+            ],
+            "art_caption": "we",
+            "art_description": "Until authority explain use. East continue quickly near material generation. Issue event ability truth space approach."
+        },
+        {
+            "id": "604adf59-2780-487a-84e0-2527c69badec",
+            "artist_name": "how",
+            "home_carousel": [
+                {
+                    "id": "32ed12b1-da3a-4010-8ced-3a17b4d2d004",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/83.png"
+                },
+                {
+                    "id": "d86d2532-8372-4c32-b723-94f9b1adf4ec",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/254.png"
+                },
+                {
+                    "id": "efc7236d-6209-480b-b85b-0238959882b8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/222.png"
+                },
+                {
+                    "id": "5a623752-0609-4549-9ab6-1fd09e033b5c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/131.png"
+                },
+                {
+                    "id": "25e15199-349c-4a68-96ba-948a5d78d657",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png"
+                }
+            ],
+            "art_caption": "accept",
+            "art_description": "Able through computer meet author southern record college. Ok international majority need study. Moment ago girl matter site shoulder edge. Region business drop win create move until."
+        },
+        {
+            "id": "30bf7dfd-988e-493e-94e7-f6dd25484924",
+            "artist_name": "mind",
+            "home_carousel": [
+                {
+                    "id": "b7107d4d-ec9f-4c67-b740-52d634e40820",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/19.png"
+                },
+                {
+                    "id": "05261767-21b5-4250-a0ab-b43445f4d30f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png"
+                },
+                {
+                    "id": "cfed7929-2167-4e00-b72e-d25240bf8d33",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/122.png"
+                },
+                {
+                    "id": "4b0fb286-b5ca-464e-9e9a-595c06749cc3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "730bcfc1-b6d6-47b3-b6de-1ff0f87f92ab",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/34.png"
+                }
+            ],
+            "art_caption": "what",
+            "art_description": "Because wear southern century. Drop chance look few team event center. Name wall situation Mrs box response process phone."
+        },
+        {
+            "id": "a418c96c-a313-410e-a3ab-bd12fcca87de",
+            "artist_name": "former",
+            "home_carousel": [
+                {
+                    "id": "568c8a0f-e0c8-4eec-8cb7-3c5b32820165",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png"
+                },
+                {
+                    "id": "1987c2ac-9949-4095-8388-219119788c41",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                },
+                {
+                    "id": "640b1aa9-ebaf-440e-bb6f-a12f712989a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/333.png"
+                },
+                {
+                    "id": "afeaa605-03fb-4a51-a852-af441a92ef79",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/195.png"
+                },
+                {
+                    "id": "ada17c3e-f26b-429c-8888-ab7b391ade22",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/286.png"
+                }
+            ],
+            "art_caption": "start",
+            "art_description": "Ground beat above general. Key organization along someone."
+        }
+    ]
+}
